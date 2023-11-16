8198db4da8c52513def84b3fef51361496697f75
Daniel Pavel <daniel.pavel19@gmail.com>
2023-11-16 11:36:34 +0200

    Main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,14 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/auth';
 import Link from 'next/link';
 import { MessagesSquareIcon } from 'lucide-react';
+import CreateChatButton from './CreateChatButton';
 
 async function Header() {
   const session = await getServerSession(authOptions);
 
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
-      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7x mx-auto">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900  max-w-7xl mx-auto">
         <Logo />
 
         <div className="flex-1 flex items-center justify-end space-x-4">
@@ -28,6 +29,7 @@ async function Header() {
             <Link href={"/pricing"} prefetch={false}></Link>
           )}
 
+          <CreateChatButton />
           <DarkModeToggle />
           <UserButton session={session} />
         </div>
